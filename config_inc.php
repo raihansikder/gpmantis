26af13c608fd40b5e003a99bf4799b29a953a33f
raihansikder <raihan.act@gmail.com>
2013-03-30 21:34:23 +0600

    First commit

---
+++ config_inc.php
@@ -0,0 +1,7 @@
+<?php
+	$g_hostname = 'localhost';
+	$g_db_type = 'mysql';
+	$g_database_name = 'gpmantis_mnts1';
+	$g_db_username = 'gpmantis_mnts1';
+	$g_db_password = '1YheZLMSIH6VqOfb';
+?>
